c32ae3646a2bc6fec8654b44a0f0dd295c031553
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-03-06 08:25:15 +0700

    add js3_pt 2.2.6_change migration:t_penjualan

--- minggu03_jobsheet3/PWL_POS/database/migrations/2025_03_06_011356_create_t_penjualan_table.php
+++ minggu03_jobsheet3/PWL_POS/database/migrations/2025_03_06_011356_create_t_penjualan_table.php
@@ -12,8 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('t_penjualan', function (Blueprint $table) {
-            $table->id();
+            $table->id('penjualan_id');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->string('pembeli', 50);
+            $table->string('penjualan_kode', 20)->unique();
+            $table->dateTime('penjualan_tanggal');
             $table->timestamps();
+
+            //Mendefinisikan ForeignKey pada kolom user_id mengacu pada kolom di tabel 'm_user'
+            $table->foreign('user_id')->references('user_id')->on('m_user')->onDelete('cascade');
         });
     }
 
