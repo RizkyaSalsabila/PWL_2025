6e4213497836815d146f67558b10bffd7514afae
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-03-06 15:37:46 +0700

    add js3_pt 2.3.10_change seeder:StokSeeder

--- minggu03_jobsheet3/PWL_POS/database/seeders/StokSeeder.php
+++ minggu03_jobsheet3/PWL_POS/database/seeders/StokSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class StokSeeder extends Seeder
 {
@@ -12,6 +13,69 @@ class StokSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [
+            [
+                'barang_id'          => 1,
+                'user_id'            => 1,
+                'stok_tanggal'       => now(),
+                'stok_jumlah'        => 15
+            ],
+            [
+                'barang_id'          => 2,
+                'user_id'            => 1,
+                'stok_tanggal'       => now(),
+                'stok_jumlah'        => 25,
+            ],
+            [
+                'barang_id'          => 3,
+                'user_id'            => 1,
+                'stok_tanggal'       => now(),
+                'stok_jumlah'        => 12,
+            ],
+            [
+                'barang_id'          => 4,
+                'user_id'            => 1,
+                'stok_tanggal'       => now(),
+                'stok_jumlah'        => 18,
+            ],
+            [
+                'barang_id'          => 5,
+                'user_id'            => 1,
+                'stok_tanggal'       => now(),
+                'stok_jumlah'        => 17,
+            ],
+            [
+                'barang_id'          => 6,
+                'user_id'            => 1,
+                'stok_tanggal'       => now(),
+                'stok_jumlah'        => 35,
+            ],
+            [
+                'barang_id'          => 7,
+                'user_id'            => 1,
+                'stok_tanggal'       => now(),
+                'stok_jumlah'        => 20,
+            ],
+            [
+                'barang_id'          => 8,
+                'user_id'            => 1,
+                'stok_tanggal'       => now(),
+                'stok_jumlah'        => 35,
+            ],
+            [
+                'barang_id'          => 9,
+                'user_id'            => 1,
+                'stok_tanggal'       => now(),
+                'stok_jumlah'        => 40,
+            ],
+            [
+                'barang_id'          => 10,
+                'user_id'            => 1,
+                'stok_tanggal'       => now(),
+                'stok_jumlah'        => 24,
+            ],
+        ];
+
+    DB::table('t_stok')->insert($data);
     }
 }
