ffb5c36cbb63eb214c47854094e7ba7b27b1581a
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-03-06 08:52:39 +0700

    add js3_pt 2.2.10_change migration:t_penjualan_detail

--- minggu03_jobsheet3/PWL_POS/database/migrations/2025_03_06_014535_create_t_penjualan_detail_table.php
+++ minggu03_jobsheet3/PWL_POS/database/migrations/2025_03_06_014535_create_t_penjualan_detail_table.php
@@ -12,8 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('t_penjualan_detail', function (Blueprint $table) {
-            $table->id();
+            $table->id('detail_id');
+            $table->unsignedBigInteger('penjualan_id')->index();
+            $table->unsignedBigInteger('barang_id')->index();
+            $table->integer('harga');
+            $table->integer('jumlah');
             $table->timestamps();
+
+            //Mendefinisikan ForeignKey pada kolom penjualan_id mengacu pada kolom di tabel 't_penjualan'
+            $table->foreign('penjualan_id')->references('penjualan_id')->on('t_penjualan')->onDelete('cascade');
+            //Mendefinisikan ForeignKey pada kolom barang_id mengacu pada kolom di tabel 'm_barang'
+            $table->foreign('barang_id')->references('barang_id')->on('m_barang')->onDelete('cascade');
         });
     }
 
