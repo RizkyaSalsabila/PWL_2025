bea929040f1095c0072986db6a3039c367626e21
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-03-06 08:11:25 +0700

    add js3_pt 2.2.4_change migration:m_barang

--- minggu03_jobsheet3/PWL_POS/database/migrations/2025_03_06_004133_create_m_barang_table.php
+++ minggu03_jobsheet3/PWL_POS/database/migrations/2025_03_06_004133_create_m_barang_table.php
@@ -12,8 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('m_barang', function (Blueprint $table) {
-            $table->id();
+            $table->id('barang_id');
+            $table->unsignedBigInteger('kategori_id')->index();
+            $table->string('barang_kode', 10)->unique();
+            $table->string('barang_nama', 100);
+            $table->integer('harga_beli');
+            $table->integer('harga_jual');
             $table->timestamps();
+
+            //Mendefinisikan ForeignKey pada kolom kategori_id mengacu pada kolom di tabel 'm_kategori'
+            $table->foreign('kategori_id')->references('kategori_id')->on('m_kategori')->onDelete('cascade');
         });
     }
 
