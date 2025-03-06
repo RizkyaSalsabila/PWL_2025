4587149518747200dbe088b7a675ee34e0b5c4f2
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-03-06 21:17:27 +0700

    add js3_pt 5.3_update data in KategoriController

--- minggu03_jobsheet3/PWL_POS/app/Http/Controllers/KategoriController.php
+++ minggu03_jobsheet3/PWL_POS/app/Http/Controllers/KategoriController.php
@@ -9,13 +9,17 @@ class KategoriController extends Controller
 {
     public function index(){
       //Menambahkan 1 data ke 'm_kategori'
-        $data = [
+       /*data = [
             'kode_kategori' => 'SNK',
             'nama_kategori' => 'Snack/Makanan Ringan',
             'created_at' => now()
         ];
 
         DB::table('m_kategori')->insert($data);
-        return 'Insert data baru berhasil';
+        return 'Insert data baru berhasil';*/
+
+        //Mengupdate data di 'm_kategori'
+        $row = DB::table('m_kategori')->where('kode_kategori', 'SNK')->update(['nama_kategori' => 'Camilan']);
+        return 'Update data berhasil, jumlah data yang diupdate: '.$row. ' baris';
     }
 }
