80919fe9eb96625bf3e1e1fffc20d1b467a06247
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-03-06 07:13:07 +0700

    add js3_pt 2.1.4_change migration:m_kategori

--- minggu03_jobsheet3/PWL_POS/database/migrations/2025_03_05_233833_create_m_kategori_table.php
+++ minggu03_jobsheet3/PWL_POS/database/migrations/2025_03_05_233833_create_m_kategori_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('m_kategori', function (Blueprint $table) {
-            $table->id();
+            $table->id('kategori_id');
             $table->string('kode_kategori', 10)->unique();              
             $table->string('nama_kategori', 120);               
             $table->text('deskripsi')->nullable(); 
