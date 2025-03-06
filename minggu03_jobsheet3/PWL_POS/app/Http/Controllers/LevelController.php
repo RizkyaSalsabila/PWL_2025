fca05f44498d41551eaa613ae71bf36a10dbc402
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-03-06 20:44:40 +0700

    add js3_pt 4.4_delete process LevelController

--- minggu03_jobsheet3/PWL_POS/app/Http/Controllers/LevelController.php
+++ minggu03_jobsheet3/PWL_POS/app/Http/Controllers/LevelController.php
@@ -13,8 +13,12 @@ class LevelController extends Controller
         // DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['CUS', 'Pelanggan', now()]);
         // return 'Insert data baru berhasil';
 
+        //KODE KEDUA
+        // $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']); 
+        // return 'Update data berhasil, jumlah data yang diupdate: '.$row. ' baris';
+
         //KODE BARU
-        $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']); 
-        return 'Update data berhasil, jumlah data yang diupdate: '.$row. ' baris';
+        $row = DB::delete('delete from m_level where level_kode = ?', ['CUS']); // Menghapus data dengan kode = 'CUS'
+        return 'Delete data berhasil, jumlah data yang dihapus: '.$row. ' baris';
     }
 }
