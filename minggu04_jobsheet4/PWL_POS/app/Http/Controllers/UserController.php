f7cbe64cfbd7dbe2de78c120f430bf0af0c100fe
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-03-09 15:39:54 +0700

    add js4_pt 1_change UserModel - UserController(1)

--- minggu04_jobsheet4/PWL_POS/app/Http/Controllers/UserController.php
+++ minggu04_jobsheet4/PWL_POS/app/Http/Controllers/UserController.php
@@ -10,18 +10,13 @@ class UserController extends Controller
 {
     public function index()
     {
-
+        // ------------------------------------- *jobsheet 03* -------------------------------------
         //tambah data user dengan Eloquent
         $data =[
             'nama' => 'Pelanggan Pertama'
         ];
 
         UserModel::where('username', 'customer-1')->update($data);  //update data user
-
-        //mencoba akses model UserModel
-        $user = UserModel::all();       //ambil semua data dari tabel 'm_user'
-        return view('user', ['data' => $user]);
-
 
         //Menambahkan data baru menggunakan Eloquent
         // $data = [
@@ -32,5 +27,21 @@ class UserController extends Controller
         // ];
 
         // UserModel::insert($data); //menambahkan data ke tabel 'm_user'
-    }
+        // -----------------------------------------------------------------------------------------
+        
+        // ------------------------------------- *jobsheet 04* -------------------------------------
+        //menambahkan data baru ke 'm_user'
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_dua',
+            'nama' => 'Manager 2',
+            'password' => Hash::make('12345')
+        ];
+
+        UserModel::create($data);
+
+        //mencoba akses model UserModel
+        $user = UserModel::all();       //ambil semua data dari tabel 'm_user'
+        return view('user', ['data' => $user]);
+    } 
 }
--- minggu04_jobsheet4/PWL_POS/app/Models/UserModel.php
+++ minggu04_jobsheet4/PWL_POS/app/Models/UserModel.php
@@ -9,6 +9,17 @@ class UserModel extends Model
 {
     use HasFactory;
 
+    // ------------------------------------- *jobsheet 03* -------------------------------------
     protected $table = 'm_user';    //mendefinisikan nama tabel yang digunakan di model ini
     protected $primaryKey = 'user_id';  //mendefinisikan primary key dari tabel yang digunakan
+    // -----------------------------------------------------------------------------------------
+
+    // ------------------------------------- *jobsheet 04* -------------------------------------
+    //menyetting agar bisa diisi ketika insert/update data
+    protected $fillable = [
+        'level_id',
+        'username', 
+        'nama',
+        'password'
+    ];
 }
