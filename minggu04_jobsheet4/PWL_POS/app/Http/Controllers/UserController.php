5c3740ce0fadab66a5572009a4969b68234470e5
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-03-12 16:18:41 +0700

    add js4_pt 2.5_change UserController(1)

--- minggu04_jobsheet4/PWL_POS/app/Http/Controllers/UserController.php
+++ minggu04_jobsheet4/PWL_POS/app/Http/Controllers/UserController.php
@@ -139,16 +139,42 @@ class UserController extends Controller
         // return view('user', ['data' => $user]);
 
         //PRAKTIKUM 2.4(5)
-        $user = UserModel::firstOrNew(          //tampilkan jika ada, jika tidak maka akan dibuatkan tapi tidak pada database
-            [
-                'username' => 'manager33',                 //dengan username = manager33
-                'nama'     => 'Manager Tiga Tiga',        //dengan nama = Manager Tiga Tiga
-                'password' => Hash::make('12345'),        //dengan password
-                'level_id' => 2                           //dengan level_id = 2
-            ],
-        );
+        // $user = UserModel::firstOrNew(          //tampilkan jika ada, jika tidak maka akan dibuatkan tapi tidak pada database
+        //     [
+        //         'username' => 'manager33',                 //dengan username = manager33
+        //         'nama'     => 'Manager Tiga Tiga',        //dengan nama = Manager Tiga Tiga
+        //         'password' => Hash::make('12345'),        //dengan password
+        //         'level_id' => 2                           //dengan level_id = 2
+        //     ],
+        // );
 
-        $user->save();          //melakukan proses penyimpanan ke database
-        return view('user', ['data' => $user]);
+        // $user->save();          //melakukan proses penyimpanan ke database
+        // return view('user', ['data' => $user]);
+
+
+        //PRAKTIKUM 2.5(1)
+        $user = UserModel::create([
+            'username'  => 'manager55',
+            'nama'      => 'Manager55',
+            'password'  => Hash::make('12345'),
+            'level_id'  => 2,
+        ]);
+        $user->username = 'manager56';
+
+        $user->isDirty();   //true
+        $user->isDirty('username');     //true
+        $user->isDirty('nama');     //false
+        $user->isDirty(['nama', 'username']);     //true
+
+        $user->isClean();   //false
+        $user->isClean('username');     //false
+        $user->isClean('nama');     //true
+        $user->isClean(['nama', 'username']);     //false
+
+        $user->save();
+
+        $user->isDirty();   //false
+        $user->isClean();   //true
+        dd($user->isDirty());
     } 
 }
