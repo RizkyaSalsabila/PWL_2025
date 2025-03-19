cfb89a08d31e45dfa3b79f5ac1a9067f64e81b59
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-03-19 14:45:22 +0700

    add js5_tugas_nomor 2

--- minggu05_jobsheet5/PWL_POS/config/adminlte.php
+++ minggu05_jobsheet5/PWL_POS/config/adminlte.php
@@ -327,6 +327,12 @@ return [
             'label' => 4,
             'label_color' => 'success',
         ],
+        //JS5 - TUGAS(2)
+        [
+            'text'  => 'Manage Kategori',
+            'url'   => '/kategori',
+            'icon'  => 'fas fa-fw fa-list',
+        ],
         ['header' => 'account_settings'],
         [
             'text' => 'profile',
