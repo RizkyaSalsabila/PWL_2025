779a53e9670efbdf5dbc7f51076ce880b700fa0a
RizkyaSalsabila <rzkysalsabila@gmail.com>
2025-04-18 12:02:45 +0700

    Implementasi JS 3-P5_Query Builder

--- minggu09_uts/POS/app/Http/Controllers/KategoriController.php
+++ minggu09_uts/POS/app/Http/Controllers/KategoriController.php
@@ -23,7 +23,30 @@ class KategoriController extends Controller
         // return 'Delete data berhasil, jumlah data yang dihapus: '.$row. ' baris';
 
         //KODE BARU
-        $data = DB::select('select * from m_kategori'); // Menampilkan semua data dari tabel 'm_kategori'
+        // $data = DB::select('select * from m_kategori'); // Menampilkan semua data dari tabel 'm_kategori'
+        // return view('kategori', ['data' => $data]);
+
+        // JS3 - P5(Query Builder)
+        //Menambahkan 1 data ke 'm_kategori'
+       /*data = [
+            'kode_kategori' => 'SNK',
+            'nama_kategori' => 'Snack/Makanan Ringan',
+            'created_at' => now()
+        ];
+
+        DB::table('m_kategori')->insert($data);
+        return 'Insert data baru berhasil';*/
+
+        //Mengupdate data di 'm_kategori'
+       /*row = DB::table('m_kategori')->where('kode_kategori', 'SNK')->update(['nama_kategori' => 'Camilan']);
+        return 'Update data berhasil, jumlah data yang diupdate: '.$row. ' baris';*/
+
+        //Menghapus data yang sudah ada di 'm_kategori'
+       /*row = DB::table('m_kategori')->where('kode_kategori', 'SNK')->delete();
+        return 'Delete data berhasil, jumlah data yang dihapus: '.$row. ' baris';*/
+
+        //Menampilkan data di 'm_kategori'
+        $data = DB::table('m_kategori')->get();
         return view('kategori', ['data' => $data]);
     }
 }
